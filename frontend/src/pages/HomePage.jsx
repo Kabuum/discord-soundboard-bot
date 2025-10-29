b498af2a04ecff84fa07ccb473305491d3349e02
Kabuum <thomsenribermartin@gmail.com>
2025-10-29 21:54:42 +0100

    crazy style update real quick

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,8 @@
 export default function HomePage() {
-    return <h1>Home Page</h1>;
+    return(
+        <div>
+            <img src={"https://media.discordapp.net/attachments/813981652455391256/1432039346034442461/GYJY-X3XsAA4pdm.png?ex=6902e5d2&is=69019452&hm=d0a9a30cd45121d077f3f0e391c44d00fe3a5ad5ae4996656d91eddc3f9ff80d&=&format=webp&quality=lossless&width=548&height=330"}/>
+            <h1>Home Page</h1>
+        </div>
+    )
 }
